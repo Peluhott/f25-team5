3d51092bd780188f5004784f1db61e1c5b6cfba9
Javier <J_sedanomar@uncg.edu>
2025-10-21 13:30:31 -0400

    pushed all files to make it easier for teammate to set up, application properties doesn't contain anything sensitive

---
+++ backend-api/src/test/java/com/example/GroupLink/GroupLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GroupLink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
