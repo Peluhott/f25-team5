c06dfffd4d44ed9cfa8829c7600860d83fa90ae6
Javier <J_sedanomar@uncg.edu>
2025-11-12 11:18:06 -0500

    refactor: change GroupController from RestController to Controller and remove unused methods

--- backend-api/src/main/java/com/example/GroupLink/Group/GroupController.java
+++ backend-api/src/main/java/com/example/GroupLink/Group/GroupController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,30 +16,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestController
+@Controller
 public class GroupController {
 
     @Autowired
     private GroupService groupService;
 
-    @GetMapping("/group/{id}")
-    public Group getGroup(@PathVariable long id) {
-        return groupService.getGroupById(id);
-    }
+    // get group
+    // add group, pass provider id in param
+    // update group, pass group id in param
+    // delete group, a post function
 
-    @PostMapping("/group/{id}")
-    public Group addGroup(@PathVariable long id, @RequestBody Group group) {
-        return groupService.createGroup(group, id);
-    }
-
-    @PutMapping("/group/{id}")
-    public Group updateGroup(@PathVariable long id, @RequestBody Group group) {
-        groupService.updateGroup(id, group);
-        return groupService.getGroupById(id);
-    }
-
-    @DeleteMapping("group/{id}")
-    public void deleteGroup(@PathVariable long id) {
-        groupService.deleteGroup(id);
-    }
 }
