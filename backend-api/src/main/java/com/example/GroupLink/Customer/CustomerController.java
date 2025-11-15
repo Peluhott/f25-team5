347ec50011cc1bc14f5f9d4d12879bdb66db4a11
IsaiahHames <isaiah.hames14@gmail.com>
2025-11-15 12:10:26 -0500

    Switched from rest controller and add mapping for customer home

--- backend-api/src/main/java/com/example/GroupLink/Customer/CustomerController.java
+++ backend-api/src/main/java/com/example/GroupLink/Customer/CustomerController.java
@@ -3,6 +3,8 @@ package com.example.GroupLink.Customer;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,15 +12,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import com.example.GroupLink.Group.GroupService;
+
+@Controller
 public class CustomerController {
     
     private CustomerService customerService;
+    private GroupService groupService;
 
-    public CustomerController(CustomerService customerService) {
+    public CustomerController(CustomerService customerService, GroupService groupService) {
         this.customerService = customerService;
+        this.groupService = groupService;
+    }
+
+    @GetMapping("/customer/home")
+    public Object getAllGroups(Model model){
+        model.addAttribute("groupList", groupService.getAllGroups());
+        model.addAttribute("title", "GroupLink");
+        return "customer/customer-home";
     }
 
     @PostMapping("/customers")
