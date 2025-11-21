6701a4cce28153458594b9e0ef0cd566f5ad7115
Javier <J_sedanomar@uncg.edu>
2025-11-20 20:55:22 -0500

    Add method to retrieve groups by provider ID and update file upload path

--- backend-api/src/main/java/com/example/GroupLink/Group/GroupRepository.java
+++ backend-api/src/main/java/com/example/GroupLink/Group/GroupRepository.java
@@ -1,9 +1,13 @@
 package com.example.GroupLink.Group;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GroupRepository extends JpaRepository<Group, Long> {
     // create group
+    List<Group> findByProvider_Id(Long providerId);
+
 }
