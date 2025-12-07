874825b23fe245692e305ca941e8cddfb0c967e5
Javier <J_sedanomar@uncg.edu>
2025-12-07 10:16:12 -0500

    Add ProviderNotification model, repository, service, and controller for managing provider notifications

---
+++ backend-api/src/main/java/com/example/GroupLink/ProviderNotification/ProviderNotificationRepository.java
@@ -0,0 +1,7 @@
+package com.example.GroupLink.ProviderNotification;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProviderNotificationRepository extends JpaRepository<ProviderNotification, Long> {
+
+}
---
+++ backend-api/src/main/java/com/example/GroupLink/ProviderNotification/ProviderNotificationService.java
@@ -0,0 +1,26 @@
+package com.example.GroupLink.ProviderNotification;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import com.example.GroupLink.CustomerNotification.CustomerNotification;
+import com.example.GroupLink.CustomerNotification.CustomerNotificationRepository;
+
+public class ProviderNotificationService {
+    private ProviderNotificationRepository providerNotificationRepository;
+
+    public ProviderNotificationService(ProviderNotificationRepository providerNotificationRepository) {
+        this.providerNotificationRepository = providerNotificationRepository;
+    }
+
+    public String formatTimestamp(LocalDateTime timestamp) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM-d");
+        return timestamp.format(formatter);
+    }
+
+    public ProviderNotification createNotification(ProviderNotification notification) {
+        LocalDateTime timestamp = LocalDateTime.now();
+        notification.setTimestamp(formatTimestamp(timestamp));
+        return providerNotificationRepository.save(notification);
+    }
+}
