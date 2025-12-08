bfc596b159a47d772d8980b00f57c9f949942074
Javier <J_sedanomar@uncg.edu>
2025-12-07 22:51:26 -0500

    Add customer notification on group membership status updates

--- backend-api/src/main/java/com/example/GroupLink/GroupMembership/GroupMembershipController.java
+++ backend-api/src/main/java/com/example/GroupLink/GroupMembership/GroupMembershipController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.GroupLink.Customer.Customer;
 import com.example.GroupLink.Customer.CustomerService;
+import com.example.GroupLink.CustomerNotification.CustomerNotification;
+import com.example.GroupLink.CustomerNotification.CustomerNotificationService;
 import com.example.GroupLink.Group.GroupService;
 
 @Controller
@@ -22,12 +25,14 @@ public class GroupMembershipController {
     private GroupMembershipService groupMembershipService;
     private CustomerService customerService;
     private GroupService groupService;
+    private CustomerNotificationService customerNotificationService;
 
     public GroupMembershipController(GroupMembershipService groupMembershipService, CustomerService customerService,
-            GroupService groupService) {
+            GroupService groupService, CustomerNotificationService customerNotificationService) {
         this.groupMembershipService = groupMembershipService;
         this.customerService = customerService;
         this.groupService = groupService;
+        this.customerNotificationService = customerNotificationService;
     }
     // create controller to update group membership status to approved or denied
 
@@ -35,6 +40,20 @@ public class GroupMembershipController {
     public Object updateStatus(@RequestParam Long membershipId, @RequestParam String status,
             @RequestParam Long providerId) {
         groupMembershipService.updateGroupMembershipStatus(membershipId, status);
+        GroupMembership mem = groupMembershipService.getGroupMembershipById(membershipId);
+        Customer customer = mem.getCustomer();
+        Long customerId = customer.getId();
+        String type = "";
+        String message = "";
+        if (status.equalsIgnoreCase("active")) {
+            type = "accepted";
+            message = "you have been accepted into a group!";
+        } else {
+            type = "rejected";
+            message = "your application was denied";
+        }
+
+        customerNotificationService.createCustomerNotification(customerId, type, message);
         return "redirect:/provider/" + providerId + "/applications";
     }
 
@@ -42,6 +61,13 @@ public class GroupMembershipController {
     public Object removeMember(@RequestParam Long membershipId, @RequestParam String status,
             @RequestParam Long providerId) {
         groupMembershipService.updateGroupMembershipStatus(membershipId, status);
+        GroupMembership mem = groupMembershipService.getGroupMembershipById(membershipId);
+        Customer customer = mem.getCustomer();
+        Long customerId = customer.getId();
+        String type = "rejected";
+        String message = "you have been removed from a group!";
+
+        customerNotificationService.createCustomerNotification(customerId, type, message);
         return "redirect:/provider/home/" + providerId;
     }
 
