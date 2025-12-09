2d5725847a557dcd9b087d7410df2e5a6a3531ea
Javier <J_sedanomar@uncg.edu>
2025-12-08 23:29:25 -0500

    Add method to retrieve notifications by provider ID in ProviderNotificationRepository

--- backend-api/src/main/java/com/example/GroupLink/ProviderNotification/ProviderNotificationRepository.java
+++ backend-api/src/main/java/com/example/GroupLink/ProviderNotification/ProviderNotificationRepository.java
@@ -1,7 +1,10 @@
 package com.example.GroupLink.ProviderNotification;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface ProviderNotificationRepository extends JpaRepository<ProviderNotification, Long> {
 
+    // return all notifications for a given provider
+    List<ProviderNotification> findByProviderId(Long providerId);
 }
